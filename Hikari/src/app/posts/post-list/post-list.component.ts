6eaa17e8ab61f12141f38c48c92c2ae5e83eb15c
JDario16 <jamonto5@eafit.edu.co>
2019-01-31 01:48:53 -0500

    Diseñando la pagina

---
+++ Hikari/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+// posts = [
+//   {tittle: 'First Post', content: 'This is the first post\'s content'},
+//   {tittle: 'Second Post', content: 'This is the second post\'s content'},
+//   {tittle: 'Third Post', content: 'This is the third post\'s content'}
+// ];
+  posts = [];
+}
