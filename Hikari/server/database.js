63ae35678a42dd0813a737a47ed96237120d368b
James Montoya <jamonto5@eafit.edu.co>
2019-02-11 18:48:12 -0500

    URL CHANGE

--- Hikari/server/database.js
+++ Hikari/server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const LUR = 'mongodb-server/hikari-gps';
+const LUR = 'mongo:27017/hikari-gps';
 
 mongoose.connect(LUR)
     .then(db => console.log('DB is connected'))
