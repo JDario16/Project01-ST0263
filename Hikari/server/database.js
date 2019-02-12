5e61619c494aca03429a0ff130940b55f7ee17f1
James <jamonto5@eafit.edu.co>
2019-02-12 13:09:19 -0500

    Coneccion local/docker

--- Hikari/server/database.js
+++ Hikari/server/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const LUR = 'mongodb://localhost/hikari-gps';
+//const LUR = 'mongodb://localhost/hikari-gps';
+const LUR = 'mongodb://database/hikari-gps';
 
 mongoose.connect(LUR)
     .then(db => console.log('DB is connected'))
